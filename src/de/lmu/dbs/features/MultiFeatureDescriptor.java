c2556511e3e467460593b295b167af423c5eeac7
Franz <franz.graf@gmail.com>
2011-07-13 07:36:55 +0000

    JavaDoc

--- src/de/lmu/dbs/features/MultiFeatureDescriptor.java
+++ src/de/lmu/dbs/features/MultiFeatureDescriptor.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 /**
  * Interface for a common descriptor that returns a <b>a set of</b> feature vectors.
- * Identifiers like pixel location, ID, etc can be encoded in the double array or 
- * in a separate object which acts as the key of the feature vector.
  * 
+ * Identifiers like pixel location, ID, etc can be encoded in the double array or 
+ * in a separate object <b>T</b> which acts as the key of the feature vector map.
  * If no special keys are required, Integers should be used as keys.
  *
  * @author graf
