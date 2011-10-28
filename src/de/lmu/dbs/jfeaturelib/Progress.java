3572dbfdbc1dfb7a1676fd7a5749c3c01e8a6513
Franz <franz.graf@gmail.com>
2011-10-28 07:54:18 +0000

    added optional progress message, added documentation

--- src/de/lmu/dbs/jfeaturelib/Progress.java
+++ src/de/lmu/dbs/jfeaturelib/Progress.java
@@ -5,25 +5,66 @@ package de.lmu.dbs.jfeaturelib;
  * updates about its progress. Valid progress values ar in the range of [0,100]. 
  * Subsequent events should have increasing progress values.
  * 
+ * Also, an optional progress message can be given.
+ * 
  * @author graf
  */
 public enum Progress {
 
-    START(0), END(100);
+    START(0, "started"), END(100, "finished");
     /**
      * Describes a progress between 0 and 100. 
      * 0 Indicates the start of a progress, 100 the end.
      */
     private final int progress;
+    private final String message;
 
+    /**
+     * Initialize the progress with an empty default message.
+     * 
+     * @param progress in [0,100]
+     * @throws IllegalArgumentException if progress is out of bounds
+     */
     Progress(int progress) {
+        this(progress, "");
+    }
+
+    /**
+     * Initialize the progress including a progress message.
+     * 
+     * @param progress in [0,100]
+     * @param message the progress message.
+     * @throws IllegalArgumentException if progress is out of bounds
+     */
+    Progress(int progress, String message) {
         if (progress < 0 || progress > 100) {
             throw new IllegalArgumentException("progress value must be in [0,100] but was " + progress);
         }
+        if (message == null) {
+            throw new NullPointerException("message must not be null");
+        }
         this.progress = progress;
+        this.message = message;
     }
 
+    /**
+     * @return progress value in [0, 100]
+     */
     public int getProgress() {
         return this.progress;
+    }
+
+    /**
+     * @return progress message (never null)
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @return classname of the progress class for use in PropertyChangeEvents
+     */
+    public static String getName() {
+        return Progress.class.getName();
     }
 }
