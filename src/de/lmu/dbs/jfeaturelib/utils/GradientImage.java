e9139dad5305154fde53f6e83b392fa524876022
Franz <franz.graf@gmail.com>
2011-11-04 18:53:54 +0000

    documentation

--- src/de/lmu/dbs/jfeaturelib/utils/GradientImage.java
+++ src/de/lmu/dbs/jfeaturelib/utils/GradientImage.java
@@ -6,62 +6,85 @@ import ij.process.FloatProcessor;
 /**
  * Class that calculates gradients from a given image.
  * 
- * The image is convolved with sobel-masks in x and y in order
+ * The image is convolved with masks (default = sobel) in x and y in order
  * to create the derivates. Afterwards, the gradient orientation
- * and gradient length is computed for each pixel.
+ * and length is computed for each pixel.
  * 
  * @author graf
  */
-public class GradientImage {
+public class GradientImage implements GradientSource {
 
-    private final FloatProcessor length, theta;
-    private final static int[] sobelX = {1, 0, -1, 2, 0, -2, 1, 0, -1};
-    private final static int[] sobelY = {1, 2, 1, 0, 0, 0, -1, -2, -1};
+    /**
+     * The kernel mask used for derivation in x-direction
+     */
+    private int[] kernelX = {1, 0, -1, 2, 0, -2, 1, 0, -1};
+    /**
+     * The kernel mask used for derivation in y-direction
+     */
+    private int[] kernelY = {1, 2, 1, 0, 0, 0, -1, -2, -1};
+    /**
+     * The processor describing the length of the gradients.
+     */
+    private FloatProcessor length;
+    /**
+     * The processor describing the orientation of the gradients in [0, Pi[.
+     */
+    private FloatProcessor theta;
 
-    public GradientImage(ImageProcessor ip) {
+    public void setIp(ImageProcessor ip) {
         ip = ip.convertToFloat();
-        ip.resetMinAndMax();
+
+        /*
+         * ipX and ipY are temporary processors for the derivations.
+         * This of course consumes quite some memory.
+         * A more optimal yet less transparent solution would be to use the 
+         * length and theta processors for derivation AND for the final
+         * result at once.
+         */
+        FloatProcessor ipX = (FloatProcessor) ip.duplicate();
+        FloatProcessor ipY = (FloatProcessor) ip.duplicate();
+        ipX.convolve3x3(kernelX);
+        ipY.convolve3x3(kernelY);
+
+//        ipX.resetMinAndMax();
+//        ipY.resetMinAndMax();
+//        new FileSaver(new ImagePlus("", ipX)).saveAsPng("c:/Temp/dx.png");
+//        new FileSaver(new ImagePlus("", ipY)).saveAsPng("c:/Temp/dy.png");
+
         length = new FloatProcessor(ip.getWidth(), ip.getHeight());
         theta = new FloatProcessor(ip.getWidth(), ip.getHeight());
 
-        FloatProcessor ipX = (FloatProcessor) ip.duplicate();
-        FloatProcessor ipY = (FloatProcessor) ip.duplicate();
-        ipX.convolve3x3(sobelX);
-        ipY.convolve3x3(sobelY);
-
-        // create length and angle processors
-        int max = ip.getWidth() * ip.getHeight();
+        final int max = ip.getWidth() * ip.getHeight();
         float dx, dy;
+        float thetaValue;
         for (int i = 0; i < max; i++) {
-            // set length
             dx = ipX.getf(i);
             dy = ipY.getf(i);
-            length.setf(i, (float) Math.sqrt(dx * dx + dy * dy));
 
-            // compute angle (theta)
-            if (dx != 0 && dy != 0) {
-                double thetaValue = Math.atan(dy / dx);
-                // make theta > 0
-                thetaValue = Double.isNaN(thetaValue) ? 0 : thetaValue;
-                thetaValue += thetaValue < 0 ? Math.PI : 0;
-                theta.setf(i, (float) thetaValue);
+            if (dx != 0 || dy != 0) {
+                length.setf(i, (float) Math.sqrt(dx * dx + dy * dy));
+                thetaValue = (float) (Math.atan2(dy, dx) + Math.PI);
+                thetaValue %= Math.PI;
+                theta.setf(i, thetaValue);
+//            } else {
+//                theta.setf(i, Float.NaN);
             }
         }
-        // is resetMinandMax really needed here?
-        length.resetMinAndMax();
-        theta.resetMinAndMax();
+
+//        length.resetMinAndMax();
+//        theta.resetMinAndMax();
+//        new FileSaver(new ImagePlus("", length)).saveAsPng("c:/Temp/len.png");
+//        new FileSaver(new ImagePlus("", theta)).saveAsPng("c:/Temp/the.png");
     }
 
     /**
-     * Returns the angle of the gradient in the area -pi through pi
-     * (see {@link Math#atan2(dy, dx)}). If you want the value between 0 and pi
-     * use: gradient += gradient &lt; 0 ? Math.PI : 0;
+     * Returns the angle of the gradient in the area [0, PI[.
      *
      * @param x x-cooridnate in pixel space
      * @param y y-cooridnate in pixel space
-     * @return angle of gradient [0-2pi]
+     * @return angle of gradient [0,pi[
      */
-    public float getTheta(int x, int y) {
+    public double getTheta(int x, int y) {
         return theta.getf(x, y);
     }
 
@@ -72,45 +95,25 @@ public class GradientImage {
      * @param y y-cooridnate in pixel space
      * @return length of gradient
      */
-    public float getLength(int x, int y) {
+    public double getLength(int x, int y) {
         return length.getf(x, y);
     }
 
-    /**
-     * creates and returns the gradient vector at this position
-     * 
-     * @TODO do we need a Gradient class?
-     * @param x
-     * @param y
-     * @return double[length, theta]
-     */
-    public double[] getGradient(int x, int y) {
-        return new double[]{getLength(x, y), getTheta(x, y)};
+    //<editor-fold defaultstate="collapsed" desc="getters & setters">
+    public int[] getKernelX() {
+        return kernelX;
     }
 
-    /**
-     * Returns the instance of the length-image.
-     * Each pixel value represents the length of the gradient vector.
-     * 
-     * Keep in mind, that a reference of the imageProcessor is returned here!
-     * You might want to call duplicate() if you want to alter the pixel data.
-     * 
-     * @return imageprocessor
-     */
-    public FloatProcessor getLength() {
-        return length;
+    public void setKernelX(int[] kernelX) {
+        this.kernelX = kernelX;
     }
 
-    /**
-     * Returns the instance of the theta-image.
-     * Each pixel value represents the angle (Theta) of the gradient vector.
-     * 
-     * Keep in mind, that a reference of the imageProcessor is returned here!
-     * You might want to call duplicate() if you want to alter the pixel data.
-     * 
-     * @return imageprocessor
-     */
-    public FloatProcessor getTheta() {
-        return theta;
+    public int[] getKernelY() {
+        return kernelY;
     }
+
+    public void setKernelY(int[] kernelY) {
+        this.kernelY = kernelY;
+    }
+    //</editor-fold>
 }
