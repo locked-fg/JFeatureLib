23e8c9c8719768829de7066e44ac6a133d342bf5
benejz <benejz@gmail.com>
2012-01-17 10:10:28 +0000

    documentation for cedd

--- src/de/lmu/dbs/jfeaturelib/features/CEDD.java
+++ src/de/lmu/dbs/jfeaturelib/features/CEDD.java
@@ -21,16 +21,40 @@ import java.util.List;
  */
 public class CEDD implements FeatureDescriptor {
 
-    public double T0;
-    public double T1;
-    public double T2;
-    public double T3;
-    public boolean Compact = false;
+    private double T0;
+    private double T1;
+    private double T2;
+    private double T3;
+    private boolean Compact = false;
+    /**
+     * 
+     */
     protected double[] data = null;
     private de.lmu.dbs.jfeaturelib.features.lire.CEDD cedd;
     private BufferedImage bi;
     private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
+    /**
+     * Constructor with default parameters
+     */
+
+    public CEDD() {
+        this.T0 = 14;
+        this.T1 = 0.68;
+        this.T2 = 0.98;
+        this.T3 = 0.98;
+        cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
+    }    
+    
+    /**
+     * Constructs new CEDD descriptor
+     * 
+     * @param Th0 Treshold for edge detection
+     * @param Th1 Treshold for non-directional edges
+     * @param Th2 Treshold for horizontal and vertical edges
+     * @param Th3 Treshold for diagonal edges
+     * @param CompactDescriptor Compact output of results
+     */
     public CEDD(double Th0, double Th1, double Th2, double Th3, boolean CompactDescriptor) {
         this.T0 = Th0;
         this.T1 = Th1;
@@ -40,45 +64,77 @@ public class CEDD implements FeatureDescriptor {
         cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
     }
 
-    public CEDD() {
-        this.T0 = 14;
-        this.T1 = 0.68;
-        this.T2 = 0.98;
-        this.T3 = 0.98;
-        cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
-    }
-
+    /**
+     * Treshold for edge detection
+     * @return Treshold for edge detection
+     */
     public double getT0(){
         return this.T0;
     }
+    /**
+     * Treshold for edge detection
+     * @param t0 Treshold for edge detection
+     */
     public void setT0(double t0){
         this.T0 = t0;
         cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
     }
+    /**
+     * Treshold for non-directional edges
+     * @return Treshold for non-directional edges
+     */
     public double getT1(){
         return this.T1;
     }
+    /**
+     * Treshold for non-directional edges
+     * @param t1 Treshold for non-directional edges
+     */
     public void setT1(double t1){
         this.T1 = t1;
         cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
     }
+    /**
+     * Treshold for horizontal and vertical edges
+     * @return Treshold for horizontal and vertical edges
+     */
     public double getT2(){
         return this.T2;
     }
+    /**
+     * Treshold for horizontal and vertical edges
+     * @param t2 Treshold for horizontal and vertical edges
+     */
     public void setT2(double t2){
         this.T2 = t2;
         cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
     }
+    /**
+     * Treshold for diagonal edges
+     * @return Treshold for diagonal edges
+     */
     public double getT3(){
         return this.T3;
     }
+    /**
+     * Treshold for diagonal edges
+     * @param t3 Treshold for diagonal edges
+     */
     public void setT3(double t3){
         this.T3 = t3;
         cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
     }
+    /**
+     * Compact output of results
+     * @return Compact output of results
+     */
     public boolean getCompact(){
         return Compact;
     }
+    /**
+     * Compact output of results
+     * @param compact Compact output of results
+     */
     public void setCompact(boolean compact){
         this.Compact = compact;
         cedd = new de.lmu.dbs.jfeaturelib.features.lire.CEDD(T0, T1, T2, T3, Compact);
