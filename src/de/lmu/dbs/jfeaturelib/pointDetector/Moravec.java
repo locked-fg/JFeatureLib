cdea278d85b6932433fa44d7a5c836c8e3e1b391
robert zelhofer <robert.zelhofer@gmail.com>
2012-01-18 11:27:23 +0000

    Moravec doc added

--- src/de/lmu/dbs/jfeaturelib/pointDetector/Moravec.java
+++ src/de/lmu/dbs/jfeaturelib/pointDetector/Moravec.java
@@ -10,19 +10,25 @@ import java.util.EnumSet;
 import java.util.List;
 
 /**
- * TODO Documentation
- * TODO add constructors
+ * 
+ * Moravec Corner Detector
+ * 
+ * Returns a List of ImagePoint Corners
+ * 
  * @author Robert Zelhofer
- * http://www.ri.cmu.edu/pub_files/pub4/moravec_hans_1980_1/moravec_hans_1980_1.pdf
+ * @see http://www.ri.cmu.edu/pub_files/pub4/moravec_hans_1980_1/moravec_hans_1980_1.pdf
  */
 public class Moravec implements PointDetector {
-    
+    //Returning List of Corners
     private List<ImagePoint> corners;
     private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
-    
+    //Threshold value
     private int threshold;
+    //windowsize value
     private int size;
     
+    //static window sizes
+    //TODO create flexible windowSize
     private final int[] xBlueDelta = new int[] { 
         -1, 0,  1, 
         -1,      1,
@@ -62,25 +68,29 @@ public class Moravec implements PointDetector {
     public void run(ImageProcessor ip) {
         pcs.firePropertyChange(Progress.getName(), null, Progress.START);
         
-        //TODO implementation
+        
         int width = ip.getWidth();
         int height = ip.getHeight();
+        //radius is used to prevent the Algorithm to search out of bounds
         int radius = size / 2;
         
+        //Check each Pixel
         for (int y = radius, maxY = height - radius; y < maxY; y++) {
             for (int x = radius, maxX = width - radius; x < maxX; x++) {
                 int minSum = Integer.MAX_VALUE;
                 
+                //check the shifting directions
                 for (int k = 0; k < 8; k++) {
+                    // sy and sx is the Center Point of the Shifted Window
                     int sy = y + yBlueDelta[k];
                     int sx = x + xBlueDelta[k];
                     
-                    
+                    // Shifted Window out of Bounds Check
                     if  (sy < radius || sx < radius || sy >= maxY || sx >= maxX) { continue;}
                     
                     int sum = 0;
                     
-                    
+                    // Sum up the difference of the window around the actual pixel and the shifted window
                     for (int i = 0; i < size; i++) {
                         for (int j = 0; j < size; j++) {
                             int redX = x + xRedDelta[i + j];
@@ -101,6 +111,7 @@ public class Moravec implements PointDetector {
                 
                 //System.out.print("\t" + minSum+ "\t|");
                 
+                //Threshold check
                 if (minSum < threshold) {
                     minSum = 0;
                 } else {
@@ -120,10 +131,29 @@ public class Moravec implements PointDetector {
         pcs.addPropertyChangeListener(listener);
     }
     
-    
+    /**
+     * Creates Moravec Detector with Default values: Threshold = 500
+     */
     public Moravec () {
-        threshold = 500;
-        size = 3;
+        this(500, 3);
+    }
+    
+    /**
+     * Creates Moravec Detector
+     * @param threshold Threshold Value, which is used for filtering uninteresting Points
+     */
+    public Moravec (int threshold) {
+        this(threshold, 3);
+    }
+    
+    /**
+     * Creates Moravec Detector
+     * @param threshold Threshold Value, which is used for filtering uninteresting Points
+     * @param size Window Size. Must be Odd. Used for Initial Search for Corners and Search for Local Maximums.
+     */
+    private Moravec (int threshold, int size) {
+        this.threshold = threshold;
+        this.size = size;
         corners = new ArrayList<>();
     }
 }
