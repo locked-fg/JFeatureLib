855b638467ae76ef9ded66a9c4f2c23bf1a88471
Franz <franz.graf@gmail.com>
2012-05-21 20:20:02 +0000

    added comments

--- src/de/lmu/dbs/jfeaturelib/utils/RGBtoGray.java
+++ src/de/lmu/dbs/jfeaturelib/utils/RGBtoGray.java
@@ -2,29 +2,60 @@ package de.lmu.dbs.jfeaturelib.utils;
 
 /**
  * Simple util to return a gray value for given RGB value
+ *
  * @author Benedikt
  */
 public class RGBtoGray {
-    
-    public static int ARGB_NTSC(int p){
+
+    /**
+     * Converts NTSC RGB to gray
+     *
+     * @param p rgb color pixel
+     * @return gray value
+     * @see http://en.wikipedia.org/wiki/Luma_%28video%29
+     */
+    public static int ARGB_NTSC(int p) {
         int r = (p & 0xff0000) >> 16;
-	int g = (p & 0xff00) >> 8;
-	int b = p & 0xff;
+        int g = (p & 0xff00) >> 8;
+        int b = p & 0xff;
         return NTSCRGBtoGray(r, g, b);
     }
-    
-    public static int ARGB_Mean(int p){
+
+    /**
+     * Converts a color pixel to gray qith a factor of 1/3 each.
+     *
+     * @param p rgb color pixel
+     * @return gray value
+     */
+    public static int ARGB_Mean(int p) {
         int r = (p & 0xff0000) >> 16;
-	int g = (p & 0xff00) >> 8;
-	int b = p & 0xff;
+        int g = (p & 0xff00) >> 8;
+        int b = p & 0xff;
         return MeanRGBtoGray(r, g, b);
     }
-    
-    public static int NTSCRGBtoGray(int r, int g, int b){
-        return (int)(0.2126*r+0.7152*g+0.0722*b);
+
+    /**
+     * Converts NTSC RGB to gray
+     *
+     * @param r red value
+     * @param g green value
+     * @param b blue value
+     * @return gray value
+     * @see http://en.wikipedia.org/wiki/Luma_%28video%29
+     */
+    public static int NTSCRGBtoGray(int r, int g, int b) {
+        return (int) (0.2126 * r + 0.7152 * g + 0.0722 * b);
     }
-    
-    public static int MeanRGBtoGray(int r, int g, int b){
-        return (r+b+g)/3;
+
+    /**
+     * Converts the rgb values to gray with equal weight
+     *
+     * @param r red value
+     * @param g green value
+     * @param b blue value
+     * @return gray value
+     */
+    public static int MeanRGBtoGray(int r, int g, int b) {
+        return (r + b + g) / 3;
     }
 }
