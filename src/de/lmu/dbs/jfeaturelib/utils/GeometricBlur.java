a514b612e1e82dd40f61a5e933964a7bce9a7b2a
Franz <franz.graf@gmail.com>
2012-05-21 20:06:44 +0000

    added comments

--- src/de/lmu/dbs/jfeaturelib/utils/GeometricBlur.java
+++ src/de/lmu/dbs/jfeaturelib/utils/GeometricBlur.java
@@ -8,41 +8,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Gaussian Blur where sigma is modified according to spacial position to
- * the "query point".
- * 
- * Can be used as a component for geometric blur. Geom.blur first separates
- * the input image into 4 channels of half-wave rectified responses from
- * oriented edge filters.
- * 
- * The idea was proposed by Alexander Berg: 
- * http://acberg.com/gb.html
+ * Gaussian Blur where sigma is modified according to spacial position to the
+ * "query point".
  *
- * Relevant papers:
- * Shape Matching and Object Recognition using Low Distortion Correspondence
- * Alexander C. Berg, Tamara L. Berg, Jitendra Malik
- * IEEE Computer Vision and Pattern Recognition (CVPR) 2005
- * (Presents a descriptor based on geometric blur and applies this to object recognition.)
- * 
- * Geometric Blur for Template Matching 
- * Alexander C. Berg, Jitendra Malik
- * Computer Vision and Pattern Recognition (CVPR) 2001, Hawaii, pp I.607-614 
- * (The original geometric blur paper concentrating on template matching and localization.)
- * 
- * Shape Matching and Object Recognition 
- * Alexander C. Berg
- * Ph.D. Thesis, Computer Science Division, U.C. Berkeley, December 2005
- * (Presents more detail on the motivation for geometric blur, study of corresponding image patches, etc.)
- * 
+ * Can be used as a component for geometric blur. Geom.blur first separates the
+ * input image into 4 channels of half-wave rectified responses from oriented
+ * edge filters.
+ *
+ * The idea was proposed by Alexander Berg: http://acberg.com/gb.html
+ *
+ * Relevant papers: Shape Matching and Object Recognition using Low Distortion
+ * Correspondence Alexander C. Berg, Tamara L. Berg, Jitendra Malik IEEE
+ * Computer Vision and Pattern Recognition (CVPR) 2005 (Presents a descriptor
+ * based on geometric blur and applies this to object recognition.)
+ *
+ * Geometric Blur for Template Matching Alexander C. Berg, Jitendra Malik
+ * Computer Vision and Pattern Recognition (CVPR) 2001, Hawaii, pp I.607-614
+ * (The original geometric blur paper concentrating on template matching and
+ * localization.)
+ *
+ * Shape Matching and Object Recognition Alexander C. Berg Ph.D. Thesis,
+ * Computer Science Division, U.C. Berkeley, December 2005 (Presents more detail
+ * on the motivation for geometric blur, study of corresponding image patches,
+ * etc.)
+ *
  * @author Graf
  */
 public class GeometricBlur {
 
-    private static final Distance distance = new DistanceL2();
+    private final Distance distance = new DistanceL2();
     private ImageProcessor ip = null;
     private double sigma = 5;
     private int blursteps = 20;
-    private List<ImageProcessor> myStack = new ArrayList<ImageProcessor>();
+    private List<ImageProcessor> myStack = new ArrayList<>();
 
     public GeometricBlur() {
     }
@@ -50,7 +48,8 @@ public class GeometricBlur {
     /**
      * @param ip image processor of source image
      * @param sig maximum sigma of gauss blur
-     * @param steps how many slices of images (with differing sigma) should be created.
+     * @param steps how many slices of images (with differing sigma) should be
+     * created.
      */
     public GeometricBlur(ImageProcessor ip, double sig, int steps) {
         this.sigma = sig;
@@ -68,13 +67,13 @@ public class GeometricBlur {
             throw new NullPointerException("ImageProcessor must not be null");
         }
 
-        final int ow = out.getWidth();
-        final int oh = out.getHeight();
-        
-        final ImagePoint p = new ImagePoint();
-        final Rectangle bound = new Rectangle(ip.getWidth(), ip.getHeight());
-        final int offsetX = (int) (center.x - ow / 2d);
-        final int offsetY = (int) (center.y - oh / 2d);
+        int ow = out.getWidth();
+        int oh = out.getHeight();
+
+        ImagePoint p = new ImagePoint();
+        Rectangle bound = new Rectangle(ip.getWidth(), ip.getHeight());
+        int offsetX = (int) (center.x - ow / 2d);
+        int offsetY = (int) (center.y - oh / 2d);
         for (int x = 0; x < ow; x++) {
             for (int y = 0; y < oh; y++) {
                 // pixel coords of window in bigger source image
@@ -88,10 +87,10 @@ public class GeometricBlur {
     }
 
     /**
-     * Returns the spacially blurred pixel value at the point q, assuming
-     * that center is the source of the blur.
-     * 
-     * @param q pixel coordante which should be retrieved
+     * Returns the spacially blurred pixel value at the point q, assuming that
+     * center is the source of the blur.
+     *
+     * @param q pixel coordinate that should be retrieved
      * @param center the source of the blur
      * @return pixel value from {@link ImageProcessor#getPixel(int, int)}
      */
@@ -100,9 +99,9 @@ public class GeometricBlur {
     }
 
     /**
-     * Returns the spacially blurred pixel value at the point q, assuming
-     * that center is the source of the blur.
-     * 
+     * Returns the spacially blurred pixel value at the point q, assuming that
+     * center is the source of the blur.
+     *
      * @param q pixel coordante which should be retrieved
      * @param center the source of the blur
      * @return pixel value from {@link ImageProcessor#getPixelValue(int, int)}
@@ -116,25 +115,25 @@ public class GeometricBlur {
         int slice = (int) dst;
         if (slice < 0) {
             slice = 0;
-        }
-        if (slice > myStack.size() - 1) {
+        } else if (slice > myStack.size() - 1) {
             slice = myStack.size() - 1;
         }
         return slice;
     }
 
+    /**
+     * create the geometric blur stack
+     */
     private void update() {
+        myStack.clear();
         if (ip == null) {
             return;
         }
-        myStack.clear();
-        // stack = new ImageStack(ip.getWidth(), ip.getHeight(), ip.getColorModel());
         // 0 = unmodified
         myStack.add(ip.duplicate());
         for (int i = 1; i <= blursteps; i++) {
             ImageProcessor blurred = ip.duplicate();
             double tmpSigma = i * sigma / (1d * blursteps);
-            // gb.blur(blurred, tmpSigma);
             GaussianBlur gb = new GaussianBlur();
             gb.blurGaussian(blurred, tmpSigma, tmpSigma, 0.01);
             myStack.add(blurred);
