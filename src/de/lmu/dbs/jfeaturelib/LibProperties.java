09ce22b4a8f1d916f6c95ddb0c199e716723cb7b
Franz <franz.graf@gmail.com>
2012-05-24 19:19:15 +0000

    changed the Properties changed according references

--- src/de/lmu/dbs/jfeaturelib/LibProperties.java
+++ src/de/lmu/dbs/jfeaturelib/LibProperties.java
@@ -1,62 +1,47 @@
 package de.lmu.dbs.jfeaturelib;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import de.lmu.ifi.dbs.utilities.PropertyContainer;
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
 /**
- * The property wrapper class for <code>jfeaturelib.properties</code>.
- * This class is used to access the properties of jfeaturelib.properties in an 
- * easier way without having to request keys by magic strings.
- * 
- * For example in order to obtain the value of <code>sift.binary</code>, you 
- * can request <code>LibProperties.SIFT_BINARY.getValue()</code>.
- * By doing so, you implicitly load the properties file and perform a call to
- * {@link Properties#getProperty(java.lang.String)} with the magic string being 
+ * The property wrapper class for
+ * <code>jfeaturelib.properties</code>. This class is used to access the
+ * properties of jfeaturelib.properties in an easier way without having to
+ * request keys by magic strings.
+ *
+ * For example in order to obtain the value of
+ * <code>sift.binary</code>, you can request
+ * <code>LibProperties.SIFT_BINARY.getValue()</code>. By doing so, you
+ * implicitly load the properties file and perform a call to
+ * {@link Properties#getProperty(java.lang.String)} with the magic string being
  * replaced by the enum.
- * 
+ *
  * @author graf
- * @see #getValue() 
+ * @see #getValue()
  * @see Properties
  */
-public enum LibProperties {
+public class LibProperties extends PropertyContainer {
 
-    /**
-     * Key identifier for the  sift binary which can be obtained from the SIFT 
-     * homepage.
-     */
-    SIFT_BINARY("sift.binary");
-    // fields 
+    private static LibProperties singleton;
     /**
      * The file name of the properties file
      */
-    private static final String BASE_FILE = "jfeaturelib.properties";
+    private static final File BASE_FILE = new File("jfeaturelib.properties");
     /**
-     * the key identifier for each property
+     * path to the sift binary which can be obtained from the SIFT homepage.
      */
-    private final String key;
+    public static final String SIFT_BINARY = "sift.binary";
 
-    LibProperties(String key) {
-        this.key = key;
+    public LibProperties() throws IOException {
+        super(BASE_FILE);
     }
 
-    /**
-     * @return the object from the property identified by this enum
-     * @throws IOException 
-     */
-    public Object getValue() throws IOException {
-        return getProperties().getProperty(this.key);
+    public static LibProperties get() throws IOException {
+        if (singleton == null) {
+            singleton = new LibProperties();
+        }
+        return singleton;
     }
-
-    // Should this be replaces by a singleton? 
-    private static Properties getProperties() throws IOException {
-        Properties properties = new Properties();
-        properties.load(new BufferedReader(new FileReader(BASE_FILE)));
-        return properties;
-    }
-
-    public String toString() {
-        return "LibProperties{" + "key=" + key + '}';
-    }
-};
+}
--- src/de/lmu/dbs/jfeaturelib/features/Sift.java
+++ src/de/lmu/dbs/jfeaturelib/features/Sift.java
@@ -12,24 +12,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * This descriptor is a wrapper class for the famous SIFT algorithm proposed by 
+ * This descriptor is a wrapper class for the famous SIFT algorithm proposed by
  * David Lowe: http://www.cs.ubc.ca/~lowe/keypoints/
- * 
- * Related papers:<br>
- * The most complete and up-to-date reference for the SIFT feature detector is given in the following journal paper:<br>
- *   David G. Lowe, "Distinctive image features from scale-invariant keypoints," International Journal of Computer Vision, 60, 2 (2004), pp. 91-110.<br>
- * The SIFT approach to invariant keypoint detection was first described in the following ICCV 1999 conference paper, which also gives some more information on the applications to object recognition:<br>
- *  David G. Lowe, "Object recognition from local scale-invariant features," International Conference on Computer Vision, Corfu, Greece (September 1999), pp. 1150-1157.<br> 
- * The following paper gives methods for performing 3D object recognition by interpolating between 2D views. It also provides a probabilistic model for verification of recognition.<br>
- *  David G. Lowe, "Local feature view clustering for 3D object recognition," IEEE Conference on Computer Vision and Pattern Recognition, Kauai, Hawaii (December 2001), pp. 682-688. [PDF]; 
  *
- * Patents:<br>
- * Method and apparatus for identifying scale invariant features in an image and use of same for locating an object in an image<br>
- *  David G. Lowe, US Patent 6,711,293 (March 23, 2004). Provisional application filed March 8, 1999. Asignee: The University of British Columbia. 
- * 
- * Keep in mind that the SIFT algorithm is patented. The SIFT binary is NOT 
+ * Related papers:<br> The most complete and up-to-date reference for the SIFT
+ * feature detector is given in the following journal paper:<br> David G. Lowe,
+ * "Distinctive image features from scale-invariant keypoints," International
+ * Journal of Computer Vision, 60, 2 (2004), pp. 91-110.<br> The SIFT approach
+ * to invariant keypoint detection was first described in the following ICCV
+ * 1999 conference paper, which also gives some more information on the
+ * applications to object recognition:<br> David G. Lowe, "Object recognition
+ * from local scale-invariant features," International Conference on Computer
+ * Vision, Corfu, Greece (September 1999), pp. 1150-1157.<br> The following
+ * paper gives methods for performing 3D object recognition by interpolating
+ * between 2D views. It also provides a probabilistic model for verification of
+ * recognition.<br> David G. Lowe, "Local feature view clustering for 3D object
+ * recognition," IEEE Conference on Computer Vision and Pattern Recognition,
+ * Kauai, Hawaii (December 2001), pp. 682-688. [PDF];
+ *
+ * Patents:<br> Method and apparatus for identifying scale invariant features in
+ * an image and use of same for locating an object in an image<br> David G.
+ * Lowe, US Patent 6,711,293 (March 23, 2004). Provisional application filed
+ * March 8, 1999. Asignee: The University of British Columbia.
+ *
+ * Keep in mind that the SIFT algorithm is patented. The SIFT binary is NOT
  * included in this lib in order to avoid violating the patent.
- * 
+ *
  * @author graf
  */
 public class Sift extends FeatureDescriptorAdapter {
@@ -41,39 +49,33 @@ public class Sift extends FeatureDescriptorAdapter {
     /**
      * Initialize the Sift wrapper with the sift binary file given in the
      * jfeaturelib.properties file.
-     * 
+     *
      * @see LibProperties#SIFT_BINARY
-     * @throws IOException 
+     * @throws IOException
      */
     public Sift() throws IOException {
-        this(new File(LibProperties.SIFT_BINARY.getValue().toString()));
+        this(LibProperties.get().getFile(LibProperties.SIFT_BINARY));
     }
 
     /**
-     * Initialize the Sift wrapper with the sift binary file.
-     * This file is called by a java process that wraps the call and parses 
-     * the output.
-     * 
-     * @param siftBinary 
+     * Initialize the Sift wrapper with the sift binary file. This file is
+     * called by a java process that wraps the call and parses the output.
+     *
+     * @param siftBinary
      */
     public Sift(File siftBinary) throws IOException {
         if (!siftBinary.canExecute()) {
-            throw new IOException("Cannot execute sift binary at: "+siftBinary.getAbsolutePath());
+            throw new IOException("Cannot execute sift binary at: " + siftBinary.getAbsolutePath());
         }
         this.siftBinary = siftBinary;
     }
 
     /**
-     * The output feature vectors are a parsed version of the SIFT-binary output.
-     * The features of each vector are assigned as follows:
-     * <ol>
-     *  <li> y-coordinate </li>
-     *  <li> x-coordinate </li>
-     *  <li> scale factor </li>
-     *  <li> rotation angle in radians</li>
-     *  <li> 128 gradient values</li>
-     * </ol>
-     * 
+     * The output feature vectors are a parsed version of the SIFT-binary
+     * output. The features of each vector are assigned as follows: <ol> <li>
+     * y-coordinate </li> <li> x-coordinate </li> <li> scale factor </li> <li>
+     * rotation angle in radians</li> <li> 128 gradient values</li> </ol>
+     *
      * @return list of sift features
      */
     @Override
@@ -91,9 +93,7 @@ public class Sift extends FeatureDescriptorAdapter {
         try {
             SiftWrapper siftWrapper = new SiftWrapper(siftBinary);
             features = siftWrapper.getFeatures(ip);
-        } catch (InterruptedException ex) {
-            log.log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
+        } catch (InterruptedException | IOException ex) {
             log.log(Level.SEVERE, null, ex);
         }
     }
