e7bf7b768ac0e5919e13cd2913b64f6cd903298f
Franz <franz.graf@gmail.com>
2012-05-24 19:23:41 +0000

    updated javadoc

--- src/de/lmu/dbs/jfeaturelib/LibProperties.java
+++ src/de/lmu/dbs/jfeaturelib/LibProperties.java
@@ -3,7 +3,6 @@ package de.lmu.dbs.jfeaturelib;
 import de.lmu.ifi.dbs.utilities.PropertyContainer;
 import java.io.File;
 import java.io.IOException;
-import java.util.Properties;
 
 /**
  * The property wrapper class for
@@ -13,31 +12,42 @@ import java.util.Properties;
  *
  * For example in order to obtain the value of
  * <code>sift.binary</code>, you can request
- * <code>LibProperties.SIFT_BINARY.getValue()</code>. By doing so, you
- * implicitly load the properties file and perform a call to
- * {@link Properties#getProperty(java.lang.String)} with the magic string being
- * replaced by the enum.
+ * <code>LibProperties.get().getFile(LibProperties.SIFT_BINARY)</code>.
  *
  * @author graf
- * @see #getValue()
- * @see Properties
+ * @see PropertyContainer
  */
 public class LibProperties extends PropertyContainer {
 
     private static LibProperties singleton;
     /**
-     * The file name of the properties file
+     * The file name of the properties file.
      */
     private static final File BASE_FILE = new File("jfeaturelib.properties");
     /**
-     * path to the sift binary which can be obtained from the SIFT homepage.
+     * Path to the sift binary which can be obtained from the SIFT homepage.
+     *
+     * {@link http://www.cs.ubc.ca/~lowe/keypoints/}
      */
     public static final String SIFT_BINARY = "sift.binary";
 
-    public LibProperties() throws IOException {
+    /**
+     * Constructor that initializes the properties container with the file
+     * defined in BASE_FILE.
+     *
+     * @see #BASE_FILE
+     * @throws IOException
+     */
+    LibProperties() throws IOException {
         super(BASE_FILE);
     }
 
+    /**
+     * Factory method for the properties container.
+     *
+     * @return an instance of the container
+     * @throws IOException
+     */
     public static LibProperties get() throws IOException {
         if (singleton == null) {
             singleton = new LibProperties();
