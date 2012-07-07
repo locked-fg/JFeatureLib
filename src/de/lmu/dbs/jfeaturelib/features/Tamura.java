adfffe27fce18a820f630422bf94fce69e2baff6
Franz <franz.graf@gmail.com>
2012-07-07 15:53:42 +0000

    moved initialization

--- src/de/lmu/dbs/jfeaturelib/features/Tamura.java
+++ src/de/lmu/dbs/jfeaturelib/features/Tamura.java
@@ -5,21 +5,16 @@ import ij.process.ImageProcessor;
 import java.util.EnumSet;
 
 /**
- * Implementation of (three) Tamura features done by Marko Keuschnig & Christian
- * Penz<br> Changes by <ul> <li> Ankit Jain (jankit87@gmail.com): histogram
- * length in set string <li> shen72@users.sourceforge.net: bugfixes in math
- * (casting and brackets) </ul> Date: 28.05.2008 Time: 11:52:03
+ * Implementation of (three) Tamura features done by Marko Keuschnig & Christian Penz
+ *
+ * Changes by <br> <ul> <li> Ankit Jain (jankit87@gmail.com): histogram length in set string <li>
+ * shen72@users.sourceforge.net: bugfixes in math (casting and brackets) </ul> Date: 28.05.2008 Time: 11:52:03
  */
 public class Tamura extends AbstractFeatureDescriptor {
 
-    private de.lmu.dbs.jfeaturelib.features.lire.Tamura tamura;
-
-    public Tamura() {
-        tamura = new de.lmu.dbs.jfeaturelib.features.lire.Tamura();
-    }
-
     @Override
     public void run(ImageProcessor ip) {
+        de.lmu.dbs.jfeaturelib.features.lire.Tamura tamura = new de.lmu.dbs.jfeaturelib.features.lire.Tamura();
         firePropertyChange(Progress.START);
         tamura.extract(ip.getBufferedImage());
         addData(tamura.getData());
@@ -28,7 +23,7 @@ public class Tamura extends AbstractFeatureDescriptor {
 
     @Override
     public String getDescription() {
-        return tamura.getStringRepresentation();
+        return "Tamura features";
     }
 
     @Override
