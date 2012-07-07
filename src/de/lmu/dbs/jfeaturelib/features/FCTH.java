a34754042a69fe1886f8ff8d809b7f55de2b1948
Franz <franz.graf@gmail.com>
2012-07-07 16:04:30 +0000

    changed instantiation

--- src/de/lmu/dbs/jfeaturelib/features/FCTH.java
+++ src/de/lmu/dbs/jfeaturelib/features/FCTH.java
@@ -5,24 +5,21 @@ import ij.process.ImageProcessor;
 import java.util.EnumSet;
 
 /**
- * The FCTH feature was created, implemented and provided by Savvas A.
- * Chatzichristofis<br/> More information can be found in: Savvas A.
- * Chatzichristofis and Yiannis S. Boutalis, <i>FCTH: Fuzzy Color and Texture
- * Histogram - A Low Level Feature for Accurate Image Retrieval</i>, in
- * Proceedings of the Ninth International Workshop on Image Analysis for
- * Multimedia Interactive Services, IEEE, Klagenfurt, May, 2008.
+ * The FCTH feature was created, implemented and provided by Savvas A. Chatzichristofis<br/> More information can be
+ * found in: Savvas A. Chatzichristofis and Yiannis S. Boutalis, <i>FCTH: Fuzzy Color and Texture Histogram - A Low
+ * Level Feature for Accurate Image Retrieval</i>, in Proceedings of the Ninth International Workshop on Image Analysis
+ * for Multimedia Interactive Services, IEEE, Klagenfurt, May, 2008.
  */
 public class FCTH extends AbstractFeatureDescriptor {
 
-    private final de.lmu.dbs.jfeaturelib.features.lire.FCTH fcth;
-
     // Constructor
     public FCTH() {
-        fcth = new de.lmu.dbs.jfeaturelib.features.lire.FCTH();
     }
 
     @Override
     public void run(ImageProcessor ip) {
+        de.lmu.dbs.jfeaturelib.features.lire.FCTH fcth = new de.lmu.dbs.jfeaturelib.features.lire.FCTH();
+
         firePropertyChange(Progress.START);
         fcth.extract(ip.getBufferedImage());
         addData(fcth.getData());
@@ -31,7 +28,7 @@ public class FCTH extends AbstractFeatureDescriptor {
 
     @Override
     public String getDescription() {
-        return fcth.getStringRepresentation();
+        return new de.lmu.dbs.jfeaturelib.features.lire.FCTH().getStringRepresentation();
     }
 
     @Override
