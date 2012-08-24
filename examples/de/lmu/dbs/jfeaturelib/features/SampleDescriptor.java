27839591bd5199e7934c00371281e48dd428c3c9
Franz <franz.graf@gmail.com>
2012-08-24 22:51:06 +0200

    added example source dir added a sample descriptor

---
+++ examples/de/lmu/dbs/jfeaturelib/features/SampleDescriptor.java
@@ -0,0 +1,38 @@
+package de.lmu.dbs.jfeaturelib.features;
+
+import de.lmu.dbs.jfeaturelib.Progress;
+import ij.process.ImageProcessor;
+import java.util.EnumSet;
+import java.util.logging.Logger;
+
+/**
+ * This is a sample descriptor
+ *
+ * @author Franz
+ */
+public class SampleDescriptor extends AbstractFeatureDescriptor {
+
+    static final Logger log = Logger.getLogger(Thumbnail.class.getName());
+
+    @Override
+    public String getDescription() {
+        return "A sample descript that just returns some arbitray numbers";
+    }
+
+    @Override
+    public EnumSet<Supports> supports() {
+        return DOES_ALL;
+    }
+
+    @Override
+    public void run(ImageProcessor ip) {
+        firePropertyChange(Progress.START);
+
+        // if you want to extract multiple features for one image just add as much 
+        // feature vectors as you want
+        double[] featureVector = {22d, 03d, 1980d};
+        addData(featureVector);
+
+        firePropertyChange(Progress.END);
+    }
+}
