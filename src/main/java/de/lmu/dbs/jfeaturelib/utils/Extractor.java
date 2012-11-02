87f58ce1b56d80ec00ae50e96106b7966329379e
Franz <info@Locked.de>
2012-11-02 21:38:09 +0100

    added first code for masking support in Extractor (not yet functional) added comments and tests

--- src/main/java/de/lmu/dbs/jfeaturelib/utils/Extractor.java
+++ src/main/java/de/lmu/dbs/jfeaturelib/utils/Extractor.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -33,17 +35,21 @@ import org.kohsuke.args4j.Option;
 public class Extractor {
 
     private static final Logger log = Logger.getLogger(Extractor.class.getName());
-    @Option(name = "--threads", usage = "amount of threads")
+    private static final int TERMINATION_TIMEOUT = 100;
+    @Option(name = "--threads", usage = "amount of threads (defaults to amount of available processors))")
     private int threads = Runtime.getRuntime().availableProcessors();
     // 
-    @Option(name = "-d", usage = "directory containing images (default: execution directory)")
-    private File directory;
+    @Option(name = "-d", aliases = {"--src-dir"}, usage = "directory containing images (default: execution directory)")
+    File imageDirectory;
     //
     @Option(name = "-r", usage = "recursively descend into directories (default: no)")
     private boolean recursive = false;
     //
-    @Option(name = "-o", usage = "output to this file (default: features.csv)")
+    @Option(name = "-o", aliases = {"--output-dir"}, usage = "output to this file (default: features.csv)")
     private File outFile;
+    //
+    @Option(name = "-m", aliases = {"--masks-dir"}, usage = "directory containing masks")
+    File maskDirectory = null;
     //
     @Option(name = "--append", usage = "append to output file (default: false = overwrite)")
     private boolean append;
@@ -51,7 +57,7 @@ public class Extractor {
     @Option(name = "-nh", usage = "omit headerline")
     private boolean omitHeader = false;
     //
-    @Option(name = "-D", usage = "use this feature descriptor (e.G: Sift)", required = true)
+    @Option(name = "-D", aliases = {"--descriptor"}, usage = "use this feature descriptor (e.G: Sift)", required = true)
     private String descriptor = null;
     //
     @Option(name = "-c", usage = "image class that should be written to the output file")
@@ -102,7 +108,7 @@ public class Extractor {
         extractor.run();
     }
 
-    private Extractor() throws IOException {
+    Extractor() throws IOException {
         properties = LibProperties.get();
         imageFormats = properties.getString(LibProperties.IMAGE_FORMATS).split(" *, *");
         for (int i = 0; i < imageFormats.length; i++) {
@@ -115,7 +121,11 @@ public class Extractor {
         openWriter();
 
         openPool();
-        processImages(createFileList());
+        Collection<File> maskList = createFileList(maskDirectory);
+        Collection<File> imageList = createFileList(imageDirectory);
+        HashMap<File, File> tuples = findTuples(imageList, maskList);
+
+        processImages(imageList);
         closePool();
 
         closeWriter();
@@ -132,8 +142,11 @@ public class Extractor {
             log.warn(ex.getMessage(), ex);
             throw new IllegalArgumentException("the descriptor class does not exist or cannot be created");
         }
-        if (directory == null || !directory.isDirectory() || !directory.canRead()) {
-            throw new IllegalArgumentException("the directory cannot be read or does not exist");
+        if (imageDirectory == null || !imageDirectory.isDirectory() || !imageDirectory.canRead()) {
+            throw new IllegalArgumentException("the source directory cannot be read or does not exist");
+        }
+        if (maskDirectory != null && (!maskDirectory.isDirectory() || !maskDirectory.canRead())) {
+            throw new IllegalArgumentException("the mask directory cannot be read or does not exist");
         }
         if (outFile == null || (outFile.exists() && !outFile.canWrite())) {
             throw new IllegalArgumentException("the output file is not valid or not writable");
@@ -151,10 +164,25 @@ public class Extractor {
         fileExists = (outFile.exists() && outFile.length() > 0);
     }
 
-    private Collection<File> createFileList() {
-        return FileUtils.listFiles(directory, imageFormats, recursive);
+    /**
+     * creates a list of image files in the specified directory and all
+     * subdirectories (if recursive is enabled)
+     *
+     * @param dir directory to start from
+     * @return a list of image files in this directory (possibly empty)
+     */
+    Collection<File> createFileList(File dir) {
+        if (dir == null) {
+            log.info("directory is null, returning empty list");
+            return Collections.EMPTY_LIST;
+        } else {
+            return FileUtils.listFiles(dir, imageFormats, recursive);
+        }
     }
 
+    /**
+     * opens the BufferedWriter which is used to write the output
+     */
     private void openWriter() {
         try {
             writer = new BufferedWriter(new FileWriter(outFile, append), WRITE_BUFFER);
@@ -164,6 +192,9 @@ public class Extractor {
         }
     }
 
+    /**
+     * closes the output writer
+     */
     private void closeWriter() {
         try {
             writer.close();
@@ -179,20 +210,34 @@ public class Extractor {
         }
     }
 
+    /**
+     * creates a new Threadpool for the image extraction tasks
+     */
     private void openPool() {
         pool = Executors.newFixedThreadPool(threads);
     }
 
+    /**
+     * closes the threadpool and awaits termination
+     */
     private void closePool() {
         try {
             pool.shutdown();
-            pool.awaitTermination(100, TimeUnit.DAYS);
+            pool.awaitTermination(TERMINATION_TIMEOUT, TimeUnit.DAYS);
         } catch (InterruptedException ex) {
             log.warn(ex.getMessage(), ex);
             throw new IllegalStateException("error while shutting down pool");
         }
     }
 
+    /**
+     * Synchronized method to write all features that were extracted from the
+     * given file to the output writer.
+     *
+     * @param file
+     * @param features
+     * @throws IOException
+     */
     private synchronized void writeOutput(File file, List<double[]> features) throws IOException {
         // we are appending to an existing file. so start with a new line
         if (lineCounter == 0 && fileExists) {
@@ -231,6 +276,61 @@ public class Extractor {
         }
     }
 
+    /**
+     * Try to find and map image files and mask files together.
+     *
+     * Thereby the relative path (starting from imageDirectory and
+     * maskDirectory) must be equal. A different file suffix is allowed. Thus,
+     * an image [imageDirectory]/classA/car.jpg can have a mask file
+     * [maskDirectory]/classA/car.png
+     *
+     * @param imageList
+     * @param maskList
+     * @return
+     */
+    HashMap<File, File> findTuples(Collection<File> imageList, Collection<File> maskList) {
+        assert imageDirectory != null : "image Directory must not be null";
+
+        HashMap<File, File> map = new HashMap<>(imageList.size());
+        String maskBasePath = maskDirectory == null ? null : maskDirectory.getAbsolutePath();
+
+        String imageBasePath = imageDirectory.getAbsolutePath();
+        String imageSuffixes = Arrays2.join(imageFormats, "|");
+        String imageSuffixReplacement = "\\.(" + imageSuffixes + ")$";
+
+        for (File imageFile : imageList) {
+            File correspondingMask = null;
+
+            if (maskDirectory != null) {
+                // get base image path starting from the imageDirectory
+                // -> foo/bar/image.jpeg
+                String imgPart = imageFile.getAbsolutePath().replace(imageBasePath, "");
+                // remove image suffix
+                // -> foo/bar/image
+                imgPart = imgPart.replaceAll(imageSuffixReplacement, "");
+
+                // search mask file
+                for (File maskFile : maskList) {
+                    String maskPath = maskFile.getAbsolutePath();
+                    // FIXME is this correct?
+                    if (maskPath.startsWith(maskBasePath + imgPart)) {
+                        correspondingMask = maskFile;
+                        break;
+                    }
+                }
+
+                // associate image with mask
+                if (correspondingMask == null) {
+                    log.warn("no mask file found for " + imageFile.getAbsolutePath());
+                }
+            }
+
+            map.put(imageFile, correspondingMask);
+        }
+
+        return map;
+    }
+
     private class Task implements Runnable {
 
         private final File file;
@@ -247,6 +347,7 @@ public class Extractor {
                 log.debug("processing file " + file.getName());
                 ImagePlus iplus = new Opener().openImage(file.getAbsolutePath());
                 ImageProcessor processor = iplus.getProcessor();
+//                ImageProcessor mask = getMask();
 
                 FeatureDescriptor fd = (FeatureDescriptor) descriptorClazz.newInstance();
                 fd.setProperties(properties);
