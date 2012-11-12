39e9c7214c9e2d06818251ed26143ba6f07f81b9
Franz <info@Locked.de>
2012-11-12 21:41:28 +0100

    Javadoc

--- src/main/java/de/lmu/ifi/dbs/jfeaturelib/ImagePoint.java
+++ src/main/java/de/lmu/ifi/dbs/jfeaturelib/ImagePoint.java
@@ -9,13 +9,25 @@ package de.lmu.ifi.dbs.jfeaturelib;
  */
 public class ImagePoint {
 
+    /**
+     * The x-coordinate of the according point.
+     */
     public double x;
+    /**
+     * The x-coordinate of the according point.
+     */
     public double y;
 
     public ImagePoint() {
     }
 
-    // constructs and initializes a point at the specified location in the coordinate plane
+    /**
+     * constructs and initializes a point at the specified location in the
+     * coordinate plane
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public ImagePoint(double x, double y) {
         this.x = x;
         this.y = y;
