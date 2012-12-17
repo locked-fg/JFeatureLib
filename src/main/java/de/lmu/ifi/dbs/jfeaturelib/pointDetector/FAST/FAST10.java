4d20151c5e16a71d91c9156f641f381961cb1714
Franz <info@Locked.de>
2012-12-17 21:41:48 +0100

    added links to Ed Rostens website in the FAST files replaced the first /** with /* so that Netbeans does not auto-reformat the licence header

--- src/main/java/de/lmu/ifi/dbs/jfeaturelib/pointDetector/FAST/FAST10.java
+++ src/main/java/de/lmu/ifi/dbs/jfeaturelib/pointDetector/FAST/FAST10.java
@@ -1,33 +1,37 @@
 /*
- * Copyright (c) 2006, 2008, 2009, 2010 Edward Rosten
- * All rights reserved.
+ * This file is part of the JFeatureLib project: http://jfeaturelib.googlecode.com
+ * JFeatureLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JFeatureLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JFeatureLib; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * You are kindly asked to refer to the papers of the according authors which 
+ * should be mentioned in the Javadocs of the respective classes as well as the 
+ * JFeatureLib project itself.
  * 
- * 	*Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
+ * Hints how to cite the projects can be found at 
+ * https://code.google.com/p/jfeaturelib/wiki/Citation
  * 
- *      *Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
+ * ##########################################################
+ * FAST Corner Detector
  * 
- * 	*Neither the name of the University of Cambridge nor the names of 
- *       its contributors may be used to endorse or promote products derived 
- *       from this software without specific prior written permission.
+ * FAST was first published By Edward Rosten in 2005, 2006:<br>
+ * - Fusing points and lines for high performance tracking (2005) and<br>
+ * - Machine learning for high-speed corner detection (2006).
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Please see http://www.edwardrosten.com/work/fast.html for links to the according papers, more information 
+ * and other reference implementations. At this page you can also find BSD-licensed code.
+ * 
+ * In case of using this code, the above copyright notive must retain the attribution to the author.
  */
 package de.lmu.ifi.dbs.jfeaturelib.pointDetector.FAST;
 
--- src/main/java/de/lmu/ifi/dbs/jfeaturelib/pointDetector/FAST/FASTUtils.java
+++ src/main/java/de/lmu/ifi/dbs/jfeaturelib/pointDetector/FAST/FASTUtils.java
@@ -1,12 +1,46 @@
+/*
+ * This file is part of the JFeatureLib project: http://jfeaturelib.googlecode.com
+ * JFeatureLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JFeatureLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JFeatureLib; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * You are kindly asked to refer to the papers of the according authors which 
+ * should be mentioned in the Javadocs of the respective classes as well as the 
+ * JFeatureLib project itself.
+ * 
+ * Hints how to cite the projects can be found at 
+ * https://code.google.com/p/jfeaturelib/wiki/Citation
+ * 
+ * ##########################################################
+ * FAST Corner Detector
+ * 
+ * FAST was first published By Edward Rosten in 2005, 2006:<br>
+ * - Fusing points and lines for high performance tracking (2005) and<br>
+ * - Machine learning for high-speed corner detection (2006).
+ * 
+ * Please see http://www.edwardrosten.com/work/fast.html for links to the according papers, more information 
+ * and other reference implementations. At this page you can also find BSD-licensed code.
+ * 
+ * In case of using this code, the above copyright notive must retain the attribution to the author.
+ */
 package de.lmu.ifi.dbs.jfeaturelib.pointDetector.FAST;
 
 /**
- *
  * @author Robert ZELHOFER
  */
-public class FASTUtils {
+class FASTUtils {
 
-    public static void make_offsets(int[] pixel, int row_stride, int x, int y) {
+    static void make_offsets(int[] pixel, int row_stride, int x, int y) {
         pixel[0] = 0 + row_stride * 3 + x + y * row_stride;
         pixel[1] = 1 + row_stride * 3 + x + y * row_stride;
         pixel[2] = 2 + row_stride * 2 + x + y * row_stride;
